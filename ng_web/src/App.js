81d8ac367556a2a904d81e7358acd06af85d0ae8
XiaorunWU <wxr571@gmail.com>
2018-11-10 11:40:13 -0500

    added bg pic, alert, title style

--- ng_web/src/App.js
+++ ng_web/src/App.js
@@ -3,19 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import {SearchBar} from "./components/searchBar"
 import { FileExplorer } from "./components/fileExplorer"
-import { Label } from 'reactstrap';
+import { Label, Badge,Alert,Button,Fade } from 'reactstrap';
 import { Constants } from './utils/constants';
+
+
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
-        <Label>{Constants.APP_NAME}</Label>
+    <Alert color="primary">
+        Your offline gateway to knowledge!
+      </Alert>
+
+      <img height="92px" src=""/>
+
+         <h1 className = "heading">{Constants.APP_NAME}</h1>
         <SearchBar/>
         <FileExplorer/>
+
       </div>
     );
   }
+
+
 }
 
 export default App;
