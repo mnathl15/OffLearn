041f043ca6e0720580a63eef419d25f678e01b6e
Jeff Xie <jeffxie4@gmail.com>
2018-11-10 11:42:04 -0500

    Changes to searchBar

--- ng_web/src/components/searchBar.js
+++ ng_web/src/components/searchBar.js
@@ -5,17 +5,27 @@ import {Utils} from "../utils/utils"
 import {Constants} from "../utils/constants"
 
 export class SearchBar extends Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            inputValue: ''
+        }; this.updateInputValue = this.updateInputValue.bind(this)
+    }
+    updateInputValue(evt){
+        this.setState({
+            inputValue: evt.target.value
+        });
+    }
     render() {
         return (
           <Row>
             <Col md={{ size: 6, offset: 3 }}>
                 <Row>
                     <Col xs="9">
-                        <Input placeholder="Search" />
+                        <Input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} placeholder="Search" />
                     </Col>
                     <Col xs="3">
-                        <Button  onClick={() => {Utils.searchQuery("test")}}>Search</Button>
+                           <Button onClick={() =>{Utils.searchQuery(this.state.inputValue)}}>Search</Button>
                     </Col>
                 </Row>
             </Col>
