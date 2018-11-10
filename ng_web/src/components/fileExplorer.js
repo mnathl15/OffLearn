62f0a4f2695e2f25611056731125c83c0279e70d
kburd <kburd@udel.edu>
2018-11-10 17:16:51 -0500

    api to backend muther quaker

--- ng_web/src/components/fileExplorer.js
+++ ng_web/src/components/fileExplorer.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, Input, Row, Col, Fade } from 'reactstrap';
 import {Utils} from "../utils/utils"
 import {Constants} from "../utils/constants"
+import axios from 'axios';
 
 
 export class FileExplorer extends Component {
     constructor(props) {
       super(props);
-      this.state = { fadeIn: true };
+      this.state = { 
+        fadeIn: true,
+        topics: []
+       };
       this.toggle = this.toggle.bind(this);
+      this.getHistory = this.getHistory.bind(this);
+      setInterval(this.getHistory, 5000)
     }
 
     toggle() {
@@ -18,12 +24,21 @@ export class FileExplorer extends Component {
       });
     }
 
+    getHistory(){
+      console.log(this.state.topics);
+      
+      axios.get(Constants.fileListUrl)
+        .then(res => {
+          this.setState({topics: res.data.data});
+        })
+    }
+
     render() {
         return (
           <div>
             <Button color="primary" onClick={this.toggle}>Toggle Fade</Button>
                 <Fade in={this.state.fadeIn} tag="h5" className="mt-3">
-                    This content will fade in and out as the button is pressed
+                  Peek a Boo
                 </Fade>
           </div>
         );
