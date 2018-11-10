4020b11a6892634ecaa843ad164622ce70cd5664
Jeff Xie <jeffxie4@gmail.com>
2018-11-10 05:43:32 -0500

    Editing crawler

--- crawler.py
+++ crawler.py
@@ -1,58 +1,31 @@
-
-
-
 from bs4 import BeautifulSoup as bs
-import requests
-
-
-
-
-
-
+import requests, os, imp
 #acts as a search engine, returns status code
 def search(text):
-
-
     html = requests.get("https://www.bing.com/search?q= " + text)
-
     page = bs(html.content,'html.parser')
-
     links = page.find_all('a')
     valid_links=[]
     link_number = 0
     for link in links:
-
         try:
             if(requests.get(link['href']).status_code==200):
                 print(link['href'])
         except:
             pass
-
-
-
-
-
-
-
-
-
-
     print(links)
-
-
-
-
-
-
-
-
-
-search("einstein")
-
-
-
-
-
+topic = imp.load_source("topic", "./models/topic.py")
+def getFileNames():
+    topicList = []
+    folderList = os.listdir("data")
+    for folder in folderList:
+        newTopic = Topic()
+        pageList = os.listdir("data/" + folder)
+        newTopic.setName(folder)
+        for page in pageList:
+            newTopic.addPage(page)
+        topicList.append(newTopic)
+    return topicList
 
 # from googlesearch import search 
 
