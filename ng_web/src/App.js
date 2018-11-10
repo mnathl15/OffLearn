1618664d53a83a356d52ba25086e5ada2ddb5177
Vishal <vcs9@case.edu>
2018-11-10 02:14:35 -0500

    Added initial UI

--- ng_web/src/App.js
+++ ng_web/src/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'reactstrap';
+import { Button, Input, Row, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 
 class App extends Component {
+
+  constructor(props) {
+   super(props);
+
+   this.toggle = this.toggle.bind(this);
+   this.state = {
+     dropdownOpen: false
+   };
+ }
+
+ toggle() {
+   this.setState({
+     dropdownOpen: !this.state.dropdownOpen
+   });
+ }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Button color="danger">danger</Button>{' '}
+          Offline Sites
         </header>
+        <body className="App-body">
+          <Input name="search" id="exampleEmail" placeholder="Search for a topic"/>
+          <Button color="info">Search</Button>{' '}
+        </body>
+        <footer className="App-footer">
+          <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle caret>
+              Topic 1
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Wikipedia</DropdownItem>
+              <DropdownItem>Encyclopedia</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </footer>
       </div>
     );
   }
