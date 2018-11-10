59c9eb359a89cb85743f0047f33baaba6045aeb7
unknown <myrealactualemail@gmail.com>
2018-11-10 03:17:53 -0500

    bing scrape

--- crawler.py
+++ crawler.py
@@ -1,99 +1,85 @@
-from bs4 import BeautifulSoup as bSoup
+
+
+from bs4 import BeautifulSoup as bs
 import requests
 
-#takes in a link, downloads the page, locates all the links, and returns the link and page
-def parser(url):
 
-    foundLinks = set()
 
-    html = requests.get(url)
-    page = bSoup(html.content, 'html5lib')
+
+
+
+
+
+#acts as a search engine, returns status code
+def search(text):
+
+
+    html = requests.get("https://www.bing.com/search?q= " + text)
+
+    page = bs(html.content,'html.parser')
 
     links = page.find_all('a')
-
-    for element in links:
-
-        try:
-            pageLink = element["href"]
-            foundLinks.add(pageLink)
-
-        except KeyError:
-            pass
-
-    return foundLinks,page
-
-def getHost(url):
-
-    host = ""
-
-    splitLink = url.split(".")
-    for i in range(1,len(splitLink)):
-        host += splitLink[i] + "."
-
-    return host[:-2]
-
-#starting link
-websiteLink = "http://www.ece.udel.edu/"
-host = getHost(websiteLink)
-
-queue = set()       #Every link we discover gets placed in here
-visited = set()     #Only visited links go in here
-pages = set()       #the raw html of every webpage goes here
-unique = set()      #the links of each unique webpage
-      
-
-
-queue.add(websiteLink)
-
-#Run until all links have been processed
-while len(queue) != 0:
-
-    link = queue.pop()
-
-    # verify the link has not yet been visited
-    if link not in visited:
+    valid_links=[]
+    link_number = 0
+    for link in links:
 
         try:
-
-            # see if the link is an extension
-            if link[:4] != "http":
-
-                # parse page and add new links to the queue
-                locatedLinks,webPage = parser( websiteLink + link)
-                queue = queue.union(locatedLinks)
-
-                # add link to visited
-                visited.add(link)
-                print(len(visited), websiteLink+link)
-
-                # check to see if page is unique
-                if webPage not in pages:
-                    pages.add(webPage)
-                    unique.add(websiteLink+link)
-
-            # see if the link is a sub domain i.e. cvorg.ece.udel 
-            elif link.find(host) != -1:
-
-                # parse page and add new links to the queue
-                locatedLinks,webPage = parser(link)
-                queue = queue.union(locatedLinks)
-
-                # add link to visited
-                visited.add(link)
-                print(len(visited), link)
-
-                # check to see if page is unique
-                if webPage not in pages:
-                    pages.add(webPage)
-                    unique.add(link)
-
-        # check for recursion error
-        except Exception as e:
+            if(requests.get(link['href']).status_code==200):
+                print(link['href'])
+        except:
             pass
 
-file = open("link.txt","w")
-for link in unique:
-    file.write(link + "\n")
-file.close()
 
-print("DONE")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    print(links)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+search("einstein")
+
+
+
+
+
+
+
+
+
+
+
+
